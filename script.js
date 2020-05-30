94d302469656f985f594431b73eac0d0804cb1e0
tarssol <tarassolyarevich@gmail.com>
2020-05-30 23:50:19 +0300

    added code example from lesson-04

---
+++ script.js
@@ -0,0 +1,62 @@
+
+/*1.*/
+// let n = +prompt('Write your number');
+
+// if(n%2 == 0){
+// 	document.write(`${n} Парне число`);
+// }
+// else{
+// 	document.write(`${n} Непарне число`);
+// }
+
+
+/*2.*/
+// let one = +prompt('Перше число'); // 7
+// let two = +prompt('Друге число'); // 12
+
+// let q = 10;
+
+// if(Math.abs(q-two)<Math.abs(q-one)){
+//   	alert(`Число ${two} ближче`);
+// }
+
+// else if(Math.abs(q-two)>Math.abs(q-one)){
+//  	alert(`Число ${one} ближче`);
+// }
+// else{
+//  	alert('Рівні');
+// }
+
+
+/*3.*/
+// for(let a = 1000; a<=1020; a+=3){
+// 	document.write(`<h2> Loop ${a}</h2>`);
+// }
+
+
+/*4.*/
+// let num = 1;
+// for(let num2 = 1; num<=55; num +=2){
+// 	console.log(num);
+// }
+
+
+/*5.*/
+function quad(a,b,c){
+	if(a==0){
+		alert('Помилка, "а"" не може дорівнювати 0');
+	}
+}
+
+
+/*6.*/ 
+// for(let a = 90; a>=0; a -=5){
+// 	document.write(`loop ${a}`);
+// }
+
+/*7.*/
+// let a = 2;
+// for(let b = 2; b<=20; b++){
+// 	document.write(`Loop ${a}`);
+// 	a += 2
+// }
